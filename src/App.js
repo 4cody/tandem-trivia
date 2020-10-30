02e28e8d53c8246538077fd5fbaffc293561146a
Cody Shaner <cshaner04@gmail.com>
2020-10-30 00:47:17 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import data from './data/Apprentice_TandemFor400_Data.json';
+
 function App() {
+  let [btnToggle, setToggle] = useState(true);
+
+  const selectQs = (k) => {
+    if (k.length === 10) return;
+
+    let n = Math.floor(Math.random() * data.length);
+
+    if (k.includes(n)) {
+      console.log(n);
+      console.log(k, 'here is the array');
+    }
+    k.push(n);
+    selectQs(k);
+    // console.log(k);
+  };
+
+  useEffect(() => {
+    selectQs([]);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='game-wrap'>
+      <button
+        className='switchBtn'
+        onClick={() => {
+          setToggle(!btnToggle);
+        }}
+      >
+        Push
+      </button>
+      <div className={btnToggle ? 'gameCube' : 'gameCube gameCubeSwitch'}>
+        <div className='screen'>Here is a screen</div>
+        <div className='screen'>2</div>
+        <div className='screen'>3</div>
+        <div className='screen'>4</div>
+      </div>
     </div>
   );
 }
